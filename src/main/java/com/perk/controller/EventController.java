810cc0d72a0d511446d36112cfc0df3f430c7de8
developersavvy9 <nigamnayak9@gmail.com>
2024-08-01 00:58:41 +0530

    Update EventController.java

--- src/main/java/com/perk/controller/EventController.java
+++ src/main/java/com/perk/controller/EventController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/communication")
+@RequestMapping("kafka/producer")
 @RequiredArgsConstructor
 public class EventController {
 
